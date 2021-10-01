23f4b7bee21195c005cc37b4b896a4ed20eed1d5
itIsEazy <emo_bmx@abv.bg>
2021-10-01 13:55:46 +0300

    Added Simulator model

---
+++ Models/Simulator/IWebSimulator.cs
@@ -0,0 +1,13 @@
+﻿namespace ActivitySimulator.Models.Simulator
+{
+    using System;
+
+    using OpenQA.Selenium;
+
+    public interface IWebSimulator
+    {
+        IWebDriver Driver { get; set; }
+
+        Random Random { get; set; }
+    }
+}
---
+++ Models/Simulator/WebSimulator.cs
@@ -0,0 +1,33 @@
+﻿namespace ActivitySimulator.Models.Simulator
+{
+    using System;
+    using System.IO;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    public class WebSimulator : IWebSimulator
+    {
+        public WebSimulator()
+        {
+            this.Driver = new ChromeDriver();
+            this.Random = new Random();
+        }
+
+        public IWebDriver Driver { get; set; }
+
+        public Random Random { get; set; }
+
+        public void SaveTextInNewFile(string path, string text)
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+        }
+
+    }
+}
