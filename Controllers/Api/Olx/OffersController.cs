1403d213f274f4b430f1fe46ad0e6c0cd3744fec
itIsEazy <emo_bmx@abv.bg>
2021-10-02 12:35:03 +0300

    API introduced and first GET method works ({searchTerm/maxPage})

---
+++ Controllers/Api/Olx/OffersController.cs
@@ -0,0 +1,30 @@
+﻿namespace ActivitySimulator.Controllers.Api.Olx
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using ActivitySimulator.Services.OLX;
+    using ActivitySimulator.Services.OLX.Models;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OffersController : ControllerBase
+    {
+        private readonly IOlxSimulator olxSimulator;
+
+        public OffersController(IOlxSimulator olxSimulator)
+        {
+            this.olxSimulator = olxSimulator;
+        }
+
+        [HttpGet("{searchTerm}/{maxPage}")]
+        public async Task<IEnumerable<MainPageOfferModel>> Get(string searchTerm, int maxPage)
+        {
+            return await this.olxSimulator.CollectAllOffersFor(searchTerm, maxPage);
+        }
+    }
+}
